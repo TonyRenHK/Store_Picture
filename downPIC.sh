c68556e6ba5fc68bbae0f8b477a9ddff499eda9e
Tony Ren <renchk@qq.com>
2018-12-27 04:49:21 +0000

    Thu Dec 27 04:49:21 UTC 2018

--- downPIC.sh
+++ downPIC.sh
@@ -2,9 +2,10 @@
 cd Blog
 
 
-wget https://snag.gy/U3lmWE.jpg -O Lightning-Components-Basics-1.jpg
-wget https://snag.gy/9kRD1a.jpg -O Lightning-Components-Basics-2.jpg
-
+wget https://snag.gy/qkLhxN.jpg -O Lightning-Components-Basics-3.jpg
+wget https://snag.gy/9kRD1a.jpg -O Lightning-Components-Basics-4.jpg
+wget https://snag.gy/L6v40g.jpg -O Lightning-Components-Basics-5.jpg
+wget https://snag.gy/61wlg9.jpg -O Lightning-Components-Basics-6.jpg
 
 cd .. 
 cd ..
