3eadb8c8b46e1047d810fccb7618bb8383180100
Tony Ren <renchk@qq.com>
2019-03-03 15:52:51 +0000

    Sun Mar  3 15:52:51 UTC 2019

--- downPIC.sh
+++ downPIC.sh
@@ -3,13 +3,13 @@ cd Blog
 
 
 
-wget https://qqadapt.qpic.cn/txdocpic/0/fa7d938279983aab2a4875354b1a2da2/0  -O lwc-4.jpg
-wget https://qqadapt.qpic.cn/txdocpic/0/6cc14055cd68277e1a5648127c59f0fd/0 -O lwc-5.jpg
-wget https://qqadapt.qpic.cn/txdocpic/0/7ef501190c75946be7fff24c027184ba/0 -O lwc-6.jpg 
+wget https://qqadapt.qpic.cn/txdocpic/0/4cf68b99ccd528dee0155a6979bbb07a/0  -O lwc-9.jpg
+wget https://qqadapt.qpic.cn/txdocpic/0/a655902b705e7c4a87ac99b86f0cf81b/0 -O lwc-10.jpg
+wget https://qqadapt.qpic.cn/txdocpic/0/e2e76dbd67919a2eec60d7cc89243386/0 -O lwc-11.jpg 
+wget https://qqadapt.qpic.cn/txdocpic/0/da2215535012b860c4c838cc74414081/0 -O lwc-12.jpg 
+wget https://qqadapt.qpic.cn/txdocpic/0/c3558bc988b34fba9aca5624f7cad1e8/0 -O lwc-13.jpg 
 
-wget https://qqadapt.qpic.cn/txdocpic/0/60db60b5bb13aa61c24da410f013603e/0 -O lwc-7.jpg 
 
-wget https://qqadapt.qpic.cn/txdocpic/0/d33dc5c0c181465ee54872cd0a256c74/0 -O lwc-8.jpg 
 
 cd .. 
 cd ..
