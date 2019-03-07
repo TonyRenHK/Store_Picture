5658a4813225047f7ada4c6b038a639e55f59756
Tony Ren <renchk@qq.com>
2019-03-07 12:37:55 +0000

    Thu Mar  7 12:37:55 UTC 2019

--- downPIC.sh
+++ downPIC.sh
@@ -1,14 +1,10 @@
 
-cd LWC
+cd Blog
 
 
 
-wget https://qqadapt.qpic.cn/txdocpic/0/2f990a66cbb4eba2b0ac31e8c525d666/0  -O lwc-1.jpg
-wget https://qqadapt.qpic.cn/txdocpic/0/f16059e28077568b5f378c5a4b55e7c1/0 -O lwc-2.jpg
-wget https://qqadapt.qpic.cn/txdocpic/0/8be8ac99df75b3e47a7ae71cc424f652/0 -O lwc-3.jpg 
-wget https://qqadapt.qpic.cn/txdocpic/0/2c165c540a491baf0f1d88fc2ffb835a/0 -O lwc-4.jpg 
-wget https://qqadapt.qpic.cn/txdocpic/0/49cbe10e4e49ee087f989a6dd8a3f3b1/0 -O lwc-5.jpg 
-wget https://qqadapt.qpic.cn/txdocpic/0/81d325ad245c0498bbeebf18834f0486/0 -O lwc-6.jpg 
+wget https://qqadapt.qpic.cn/txdocdl/2700585860/7B19093EDF282C397802A2F117EBB079/0  -O Lightning-Components-Basics-16.jpg
+
 
 
 
